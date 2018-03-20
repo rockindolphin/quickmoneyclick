9c5364234d74dba06d6b880ab33e1879bfa5fef9
Unknown <rockindolphing@gmail.com>
2018-03-20 10:57:26 +0300

    svg images translate

---
+++ src/js/languages.js
@@ -0,0 +1,262 @@
+var arrLang = {
+	'ru': {
+		'login': 'Войти',
+		'signin': 'Зарегистрироваться',
+		'business': 'Бизнес-аккаунт',
+		'personal': 'Ваш персональный счет',
+		'mutual_settlements': 'Взаиморасчёты',
+		'currency_calculation': 'Валюта расчёта доллар, евро, чешская крона',
+		'business_segments': 'Все сегменты бизнеса',
+		'personalized_approach': 'Персонализированный подход',
+		'learn_more': 'Узнать больше',
+		'open_account': 'Открыть аккаунт',
+		'blockchain': 'Блокчейн технологии',
+		'transactions': 'Технологичные транзакции',
+		'replenishment': 'Пополнение аккаунта USD и EUR криптовалютой',
+		'crypto_address': 'Выплаты на криптоадреса',
+		'own_crypto': 'Генерация собственного криптоадреса',
+		'auto_convert': 'Автоконвертация криптовалюты в USD и EUR',
+		'merchant_account': 'Мерчант-аккаунт',
+		'solutions': 'Решения для Вашего бизнеса',
+		'receiving_payments': 'Прием онлайн платежей на Ваш ресурс со всего мира',
+		'reverse_payouts': 'Обратные выплаты',
+		'different_business': 'Работа с различными видами бизнеcа',
+		'customized_solutions': 'Индивидуальные решения',
+		'features': 'Возможности QMC',
+		'better_than_bank': 'Лучше, чем банк!',
+		'account_opening': 'Открытие аккаунта',
+		'account_replenishment': 'Пополнение аккаунта',
+		'withdrawal_of_funds': 'Вывод средств',
+		'instant_translations': 'Мгновенные переводы',
+		'electronic_wallet': 'Электронный кошелек для бизнес решений',
+		'commissions': 'Комиссии и сборы',
+		'terms': 'Условия и положения',
+		'policy': 'Политика конфиденциальности',
+		'copy_1': 'Электронные кошельки предоставляются: FSV Money Pte. Ltd (Сингапур), владеющий правом  stored value facility (SVF).',
+		'copy_2': 'Регистрационный номер 201619216W. FATCA GIIN:CZCKZE.99999.SL.702',
+		'popup4_list1': 'Принимайте криптовалюту за товары, работы, услуги без открытия криптоаккаунта. Риски большой волатильности курса отсутствуют.',
+		'popup4_list2': 'При получении криптовалюты она по текущему курсу отображается у вас на аккаунте в долларах или евро.',
+		'popup4_list3': 'Рассчитывайтесь в криптовалюте без наличия криптоаккаунта. У вас есть на аккаунте QMC доллары или евро: подайте заявку на вывод с указанием криптоадреса вашего продавца.',
+		'start_work': 'Откройте счет и начните работу',
+		'our_capabilities': 'Наши возможности',
+		'account_opening2': 'Открытие аккаунта. Доллары, Евро, Чешская крона.',
+		'account_replenishment2': 'Пополнение аккаунта: банковской картой, банковским переводом, криптовалютой.',
+		'withdrawal_of_funds2': 'Вывод с аккаунта: на банковскую карту, на банковский счёт или на криптоаккаунт.',
+		'instant_translations2': 'Мгновенные переводы. Внутри аккаунта - между своими счетами. Между аккаунтами в системе QMC.',
+		'merchant_open': 'Открыть мерчант аккаунт',
+		'popup3-2_list1': 'Кликните по ссылке ниже, чтобы скачать форму заявки.',
+		'popup3-2_list2': 'Заполните ее и отправьте на почту info@quickmoneyclik.com',
+		'popup3-2_list3': 'Дождитесь одобрения заявки.',
+		'popup3-2_list4': 'Получите API для интеграции на Ваш сайт.',
+		'popup3-2_list5': 'Начните получать платежи и делать выплаты на своем сайте.',
+		'popup3-2_list6': 'Скачать форму',
+		'merchant_account3': 'Мерчант аккаунт',
+		'merchant_for_business': 'Счет торговца для бизнеса в интернете в системе QMC',
+		'popup3_list1': 'Обычный аккаунт с одной валютой и статусом мерчант',
+		'popup3_list2': 'Неограниченные возможности по выплатам с мерчант аккаунта',
+		'popup2_title': 'Решения для бизнеса',
+		'popup2_subtitle': 'Взаиморасчеты в сегментах b2b, b2p, p2p',
+		'popup2_list1': 'Взаиморасчеты между покупателем и продавцом без участия банковских структур.',
+		'popup2_list2': 'Выплаты за работы, услуги',
+		'popup2_list3': 'Оптимизация налогообложения при использования QMC',
+		//-schemes
+		'scheme-2_1-1': 'Кошелёк покупателяКошелёк продавца',
+		'scheme-2_1-2': 'ПродавецПокупатель',
+
+		'scheme-2_2-1': 'Работодатель',
+		'scheme-2_2-2': 'Фрилансер',
+		'scheme-2_2-3': 'Дизайнер',
+		'scheme-2_2-4': 'Строитель',
+		
+		'scheme-2_3-1': 'ДоПосле',
+
+		'scheme-3_1-1': 'Запрос на оплату',
+		'scheme-3_1-2': 'через QMC',
+		'scheme-3_1-3': 'Перевод между',
+		'scheme-3_1-4': 'аккаунтами внутри QMC',
+		'scheme-3_1-5': 'Мерчант аккаунт',
+		'scheme-3_1-6': 'Кошельки клиентов QMC',
+		'scheme-3_1-7': 'Сайт продавца',
+		'scheme-3_1-8': 'Интеграция Мерчант аккаунта по API',
+		'scheme-3_1-9': 'Уведомление продавцу о поступлении средств',
+
+		'scheme-3_2-1': 'Мерчант аккаунт',
+		'scheme-3_2-2': 'Кошельки клиентов QMC',
+
+		'scheme-3_2_1-1': 'Все виды бизнеса',
+		'scheme-3_2_1-2': 'Удобные способы',
+		'scheme-3_2_1-3': 'снятия и пополнения',
+		'scheme-3_2_1-4': 'Выберите валюту:',
+		'scheme-3_2_1-5': 'USD или EURO',
+		'scheme-3_2_1-6': 'Минимальные',
+		'scheme-3_2_1-7': 'размеры комиссии',
+
+		'scheme-4_1-1': 'Аккаунт в QMC',
+		'scheme-4_1-2': 'Генератор',
+		'scheme-4_1-3': 'QR кода',
+		'scheme-4_1-4': 'Отправьте',
+		'scheme-4_1-5': 'криптоадрес',
+		'scheme-4_1-6': 'покупателю',
+		'scheme-4_1-7': 'Покупатель',
+		'scheme-4_1-8': 'оплачивает',
+		'scheme-4_1-9': 'в криптовалюте',
+		'scheme-4_1-10': 'Автоперевод криптовалюты в USD и EUR',
+
+		'scheme-4_2-1': 'Автоконвертация',
+
+		'scheme-4_3-1': 'Аккаунт в QMC',
+		'scheme-4_3-2': 'Заявка на вывод',
+		'scheme-4_3-3': 'USD или EUR',
+		'scheme-4_3-4': 'на криптоадресс',
+		'scheme-4_3-5': 'Продавец',
+		'scheme-4_3-6': 'получает',
+		'scheme-4_3-7': 'криптовалюту',
+	},
+	'cze': {
+		'login': 'Vstup',
+		'signin': 'Registrace',
+		'business': 'Obchodní účet',
+		'personal': 'Váš osobní účet',
+		'mutual_settlements': 'Vzájemné vyrovnání',
+		'currency_calculation': 'Zúčtovací měna účtu je dolar, euro, česká koruna',
+		'business_segments': 'Všechny segmenty podnikání',
+		'personalized_approach': 'Osobní přístup',
+		'learn_more': 'Chci se dozvědět víc',
+		'open_account': 'Otevřít si účet',
+		'blockchain': 'Technologie blockchain',
+		'transactions': 'Technologické transakce',
+		'replenishment': 'Vklad kryptoměny na USD a EUR účet',
+		'crypto_address': 'Platba na krypto adresy',
+		'own_crypto': 'Vygenerování vlastní krypto adresy',
+		'auto_convert': 'Automatický převod měny na USD a EUR',
+		'merchant_account': 'Účet obchodníka',
+		'solutions': 'Řešení pro Vaše podnikání',
+		'receiving_payments': 'Příjem online plateb z celého světa na Váš zdroj',
+		'reverse_payouts': 'Zpětné platb',
+		'different_business': 'Práce s různými druhy podnikání',
+		'customized_solutions': 'Individuální řešení',
+		'features': 'Možnost QMC',
+		'better_than_bank': 'Více než banka!',
+		'account_opening': 'Otevření účtu',
+		'account_replenishment': 'Vklad na účet',
+		'withdrawal_of_funds': 'Výběr prostředků',
+		'instant_translations': 'Okamžité převody',
+		'electronic_wallet': 'Elektronická peněženka pro podnikatelská řešení',
+		'commissions': 'Provize a poplatky',
+		'terms': 'Podmínky a okolnosti',
+		'policy': 'Zachování mlčenlivosti',
+		'copy_1': 'Elektronické peněženky jsou nabízeny společností FSV Money Pte. Ltd (Singapur) s právem stored value facility (SVF). ',
+		'copy_2': 'Registrační číslo 201619216W. FATCA GIIN:CZCKZE.99999.SL.702',
+		'popup4_list1': 'Obdržte kryptoměnu za zboží, práci a služby i bez otevření krypto účtu. Nulové riziko vysoké volatility kurzu.',
+		'popup4_list2': 'Při obdržení se kryptoměna zobrazí na vašem účtu v dolarech nebo eurech podle aktuálního kurzu.',
+		'popup4_list3': 'Plaťte v kryptoměně i bez krypto účtu. Na vašem účtu QMC jsou dolary nebo eura: podejte žádost o odeslání na krypto adresu vašeho prodejce.',
+		'start_work': 'Otevřete si účet a začněte pracovat',
+		'our_capabilities': 'Naše možnosti',
+		'account_opening2': 'Otevření účtu. Dolary, eura, česká koruna',
+		'account_replenishment2': 'Vklad na účet: prostřednictvím bankovní karty, bankovního převodu, kryptoměny',
+		'withdrawal_of_funds2': 'Výběr z účtu: na bankovní kartu, bankovní účet nebo krypto účet',
+		'instant_translations2': 'Okamžité převody. V rámci účtu - mezi vlastními účty. Mezi účty v systému QMC',
+		'merchant_open': 'Otevřít obchodní účet',
+		'popup3-2_list1': 'Pro stažení žádosti klikněte na níže uvedený odkaz.',
+		'popup3-2_list2': 'Vyplňte žádost a odešlete ji na adresu info@quickmoneyclick.com',
+		'popup3-2_list3': 'Počkejte na schválení žádosti.',
+		'popup3-2_list4': 'Obdržíte API kvůli integraci na Vaše stánky.',
+		'popup3-2_list5': 'Začněte přijímat a provádět platby na svých stránkách.',
+		'popup3-2_list6': 'Stáhnout formulář',
+		'merchant_account3': 'Obchodní účet',
+		'merchant_for_business': 'Účet obchodníka pro podnikání na internetu v systému QMC.',
+		'popup3_list1': 'Běžný účet s jednou měnou a statusem obchodníka.',
+		'popup3_list2': 'Neohraničené možnosti plateb z obchodního účtu.',
+		'popup2_title': 'Podnikatelská řešení.',
+		'popup2_subtitle': 'Vzájemné vyrovnání v modelech b2b, b2p, p2p',
+		'popup2_list1': 'Vzájemné vyrovnání mezi kupujícím a prodejcem bez účasti bankovních struktur',
+		'popup2_list2': 'Platba za práci, služby',
+		'popup2_list3': 'Optimalizace zdanění při používání QMC',
+		//-schemes
+		'scheme-2_1-1': 'Peněženka kupujícího Peněženka prodejce',
+		'scheme-2_1-2': 'Prodejce Kupující',
+
+		'scheme-2_2-1': 'Zaměstnavatel',
+		'scheme-2_2-2': 'Živnostník',
+		'scheme-2_2-3': 'Návrhář',
+		'scheme-2_2-4': 'Stavitel',
+
+		'scheme-2_3-1': 'DoPo',
+		
+		'scheme-3_1-1': 'Požadavek na platbu',
+		'scheme-3_1-2': 'prostřednictvím QMC',
+		'scheme-3_1-3': 'Převod mezi',
+		'scheme-3_1-4': 'účty v rámci QMC',
+		'scheme-3_1-5': 'Obchodní účet',
+		'scheme-3_1-6': 'Peněženky klientů QMC',
+		'scheme-3_1-7': 'Stránky prodejce',
+		'scheme-3_1-8': 'Integrace obchodního účtu podle API',
+		'scheme-3_1-9': 'Oznámení o navýšení prostředků prodejci',
+
+		'scheme-3_2-1': 'Obchodní účet',
+		'scheme-3_2-2': 'Peněženky klientů QMC',
+
+		'scheme-3_2_1-1': 'Všechny druhy podnikání',
+		'scheme-3_2_1-2': 'Pohodlný způsob',
+		'scheme-3_2_1-3': 'výběru a vkladu',
+		'scheme-3_2_1-4': 'Vyberte měnu:',
+		'scheme-3_2_1-5': 'USD nebo EUR',
+		'scheme-3_2_1-6': 'Minimální',
+		'scheme-3_2_1-7': 'výše provize',
+
+		'scheme-4_1-1': 'Účet QMC',
+		'scheme-4_1-2': 'Generátor',
+		'scheme-4_1-3': 'QR kódu',
+		'scheme-4_1-4': 'Pošlete',
+		'scheme-4_1-5': 'krypto adresu',
+		'scheme-4_1-6': 'kupujícímu',
+		'scheme-4_1-7': 'Kupující',
+		'scheme-4_1-8': 'platí',
+		'scheme-4_1-9': 'v krypto měně',
+		'scheme-4_1-10': 'Automatický převod kryptoměny na USD a EUR',
+
+		'scheme-4_2-1': 'Automatický převod',
+
+		'scheme-4_3-1': 'Účet v QMC',
+		'scheme-4_3-2': 'Žádost o odeslání',
+		'scheme-4_3-3': 'USD nebo EUR',
+		'scheme-4_3-4': 'na krypto adresu',
+		'scheme-4_3-5': 'Prodejce',
+		'scheme-4_3-6': 'obdrží',
+		'scheme-4_3-7': 'kryptoměnu',
+	},
+	'en': {
+		
+	}
+};
+
+
+
+(function($){
+	$(document).ready(function() {
+
+		function toggleLangList(lang){
+			$('.header__languages .link--active').removeClass('link--active');
+			$('.header__languages .link[data-lang='+lang+']').addClass('link--active');
+			$.cookie("default_lang", lang);
+		}
+
+		function translate(lang){
+			toggleLangList(lang);
+			$('.lang').each(function(index, element) {
+				$(this).text(arrLang[lang][$(this).attr('data-key')]);
+			});
+		}		
+
+		var lang = $.cookie("default_lang") || 'ru';
+		lang !== 'ru' ? translate(lang) : false;
+
+		$('.translate').click(function(e) {
+			translate( $(this).attr('data-lang') );
+			e.preventDefault();
+		});
+
+	});	
+
+})(jQuery);		
+
